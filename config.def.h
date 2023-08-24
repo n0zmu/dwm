aa8d88addf472221093fdaa6cc31b02a2ebfa6b2
n0zmu <mickael.munozserrano@gmail.com>
2023-08-24 12:38:43 +0200

    togglebar key

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = brwsrcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_n,      togglebar,      {0} },
 	{ MODKEY,                       XK_space,  focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
