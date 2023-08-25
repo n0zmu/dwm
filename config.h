2879074b8b7b57c9799ae1c339d17cd077f342d6
n0zmu <mickael.munozserrano@gmail.com>
2023-08-25 10:52:26 +0200

    dmenu clrs

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11" };
+static const char *fonts[]          = { "JetBrains Mono:size=11", "siji:size=11" };
 static const char dmenufont[]       = "JetBrains Mono:size=11";
 static unsigned int baralpha        = 250;
 static unsigned int borderalpha     = OPAQUE;
@@ -14,6 +14,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#95BDFF";
+static const char col_pink[]	    = "#F7C8E0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_cyan, col_gray1, col_gray2 },
@@ -32,6 +33,7 @@ static const Rule rules[] = {
 	{ "Chromium", NULL,	  NULL,	      1 << 1,	    0,		 -1 },
 	{ "Spotify",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Burp",     NULL,	  NULL,	      1 << 2,	    1,		 -1 },
+	{ "Steam",    NULL,	  NULL,	      1 << 7,	    0,		 -1 },
 };
 
 /* layout(s) */
@@ -39,10 +41,6 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const char *lockcmd[]  = { "slock",  NULL };
-static const char *upvol[]    = { "/usr/bin/pactl", "set-sink-volume", "1", "+5%",     NULL };
-static const char *downvol[]  = { "/usr/bin/pactl", "set-sink-volume", "1", "-5%",     NULL };
-static const char *mutevol[]  = { "/usr/bin/pactl", "set-sink-mute",   "1", "toggle",  NULL };
 
 #include "tatami.c"
 static const Layout layouts[] = {
@@ -67,10 +65,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brwsrcmd[] = { "chromium", NULL };
 static const char *strmcmd[]  = { "spotify", NULL };
+static const char *lockcmd[]  = { "slock",  NULL };
+static const char *upvol[]    = { "/usr/bin/pactl", "set-sink-volume", "1", "+5%",     NULL };
+static const char *downvol[]  = { "/usr/bin/pactl", "set-sink-volume", "1", "-5%",     NULL };
+static const char *mutevol[]  = { "/usr/bin/pactl", "set-sink-mute",   "1", "toggle",  NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
