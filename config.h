b675187f52c877d9f7c397d904849db9de6065a9
n0zmu <mickael.munozserrano@gmail.com>
2023-08-26 23:15:48 +0200

    colors

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11", "siji:size=11" };
+static const char *fonts[]          = { "JetBrains Mono:size=11", "FontAwesome:size=12:antialias=true;autohint=true" };
 static const char dmenufont[]       = "JetBrains Mono:size=11";
 static unsigned int baralpha        = 250;
 static unsigned int borderalpha     = OPAQUE;
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_pink, "-sb", col_pink, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brwsrcmd[] = { "chromium", NULL };
 static const char *strmcmd[]  = { "spotify", NULL };
