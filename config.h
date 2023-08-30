4f54c365326766dd41c7c0fbbebf333a1bf290b9
n0zmu <mickael.munozserrano@gmail.com>
2023-08-31 00:06:32 +0200

    polish

--- config.h
+++ config.h
@@ -5,20 +5,20 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=11", "FontAwesome:size=12:antialias=true;autohint=true" };
-static const char dmenufont[]       = "JetBrains Mono:size=11";
-static unsigned int baralpha        = 250;
+static const char *fonts[]          = { "hack:size=11", "FontAwesome:size=11:antialias=true;autohint=true" };
+static const char dmenufont[]       = "hack:size=11";
+static unsigned int baralpha        = 180;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#F9F9F9";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#95BDFF";
-static const char col_pink[]	    = "#F7C8E0";
+static const char col_green[]       = "#CDF0EA";
+static const char col_pink[]        = "#F7C8E0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_cyan, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray1, col_green,  col_green  },
 };
 
 /* tagging */
