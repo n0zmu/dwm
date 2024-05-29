d1b6a2c609d997def89a71f4cb4552e45ebd99f4
n0zmu <mickael.munozserrano@gmail.com>
2024-05-29 19:28:31 +0200

    dmenu

--- config.def.h
+++ config.def.h
@@ -14,11 +14,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#feffac";
+static const char col_yellow[]      = "#feffac";
+static const char col_pink[]        = "#f7c8e0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray1, col_yellow,  col_yellow  },
 };
 
 /* tagging */
@@ -62,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brwsrcmd[] = { "chromium", NULL };
  static const char *strmcmd[]  = { "audacious", NULL };
