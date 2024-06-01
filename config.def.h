ce8ea4563ea21d33df2631693303d432fca4249d
n0zmu <mickael.munozserrano@gmail.com>
2024-06-01 09:26:22 +0200

    orange

--- config.def.h
+++ config.def.h
@@ -14,8 +14,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_yellow[]      = "#feffac";
-static const char col_pink[]        = "#f7c8e0";
+static const char col_orange[]      = "#ffb4b4";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
